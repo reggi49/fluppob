b13bb5b94a8b95c9c735cf6e456b9a6c1bdbcdb9
reggi49 <reggimuhamad@gmail.com>
2023-07-05 16:56:48 +0700

    progress home banner

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter/rendering.dart';
 import 'package:get/get.dart';
-
 import '../controllers/home_controller.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
@@ -11,9 +11,9 @@ class HomeView extends GetView<HomeController> {
     return Scaffold(
         appBar: AppBar(
           title: RichText(
-              text: const TextSpan(text: "Hai", children: [
+              text: const TextSpan(text: "Hai, ", children: [
             TextSpan(
-                text: "Mhamad",
+                text: "Muhamad",
                 style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold))
           ])),
           actions: [
@@ -23,7 +23,8 @@ class HomeView extends GetView<HomeController> {
                 margin: const EdgeInsets.only(right: 30),
                 width: 30,
                 height: 30,
-                child: Image.asset("name", fit: BoxFit.contain),
+                child:
+                    const FaIcon(FontAwesomeIcons.qrcode, color: Colors.white),
               ),
             )
           ],
@@ -36,20 +37,144 @@ class HomeView extends GetView<HomeController> {
             ClipPath(
               clipper: ClipPathClass(),
               child: Container(
-                height: 250,
+                height: 230,
                 width: Get.width,
                 color: Colors.red,
               ),
             ),
             Container(
-              margin: EdgeInsets.only(top: 10),
-              color: Colors.amber,
+              margin: const EdgeInsets.only(top: 20),
               child: Column(
                 children: [
                   Container(
-                    height: Get.height * 0.3,
+                    height: Get.height * 0.42,
+                    child: LayoutBuilder(
+                        builder: (context, constraints) => Column(
+                              children: [
+                                ClipPath(
+                                    clipper: ClipInfoClass(),
+                                    child: Container(
+                                      padding: const EdgeInsets.all(15),
+                                      margin: const EdgeInsets.symmetric(
+                                          horizontal: 25),
+                                      // height: 230,
+                                      decoration: const BoxDecoration(
+                                          gradient: LinearGradient(colors: [
+                                        Color(0xFFE52D27),
+                                        Color(0xFFB31217),
+                                      ])),
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Row(children: [
+                                            const Text(
+                                              "0121829821",
+                                              style: TextStyle(
+                                                  color: Colors.white,
+                                                  fontSize: 20,
+                                                  fontWeight: FontWeight.bold),
+                                            ),
+                                            Image.asset(
+                                                "assets/icons/SimPATI_Logo.png")
+                                          ]),
+                                          const SizedBox(height: 10),
+                                          const Text(
+                                            "Sisa Pulsa Anda",
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 15,
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                          const SizedBox(height: 10),
+                                          Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.spaceBetween,
+                                            children: [
+                                              const Text(
+                                                "Rp 34.000",
+                                                style: TextStyle(
+                                                    color: Colors.white,
+                                                    fontSize: 25,
+                                                    fontWeight:
+                                                        FontWeight.bold),
+                                              ),
+                                              ElevatedButton(
+                                                onPressed: () {},
+                                                style: ElevatedButton.styleFrom(
+                                                    backgroundColor:
+                                                        Colors.amber),
+                                                child: const Text(
+                                                  "Isi Pulsa",
+                                                  style: TextStyle(
+                                                      color: Colors.black,
+                                                      fontSize: 20,
+                                                      fontWeight:
+                                                          FontWeight.bold),
+                                                ),
+                                              )
+                                            ],
+                                          ),
+                                          const SizedBox(height: 10),
+                                          RichText(
+                                              text: const TextSpan(
+                                            text: "Berlaku seumur ",
+                                            style: TextStyle(
+                                                color: Colors.black,
+                                                fontSize: 16),
+                                            children: [
+                                              TextSpan(
+                                                  text: "hidup",
+                                                  style: TextStyle(
+                                                      color: Colors.white,
+                                                      fontSize: 16))
+                                            ],
+                                          )),
+                                          const SizedBox(height: 10),
+                                          Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.start,
+                                            children: [
+                                              const Text(
+                                                "Telkomsel Point",
+                                                style: TextStyle(
+                                                    color: Colors.white,
+                                                    fontSize: 16),
+                                              ),
+                                              SizedBox(width: 10),
+                                              Container(
+                                                padding:
+                                                    const EdgeInsets.symmetric(
+                                                        horizontal: 10,
+                                                        vertical: 5),
+                                                decoration: BoxDecoration(
+                                                  color: Colors.amber,
+                                                  borderRadius:
+                                                      BorderRadius.circular(10),
+                                                ),
+                                                child: const Text(
+                                                  "172",
+                                                  style: TextStyle(
+                                                      fontSize: 16,
+                                                      color: Colors.black,
+                                                      fontWeight:
+                                                          FontWeight.bold),
+                                                ),
+                                              )
+                                            ],
+                                          ),
+                                          const SizedBox(height: 10),
+                                        ],
+                                      ),
+                                    )),
+                              ],
+                            )),
+                  ),
+                  Container(
+                    height: 10,
                     color: Colors.purple,
-                  )
+                  ),
+                  Expanded(child: Container(color: Colors.blue))
                 ],
               ),
             )
@@ -62,11 +187,28 @@ class ClipPathClass extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
     var path = Path();
-    path.lineTo(0, size.height - 30);
+    path.lineTo(0, size.height - 60);
 
     path.quadraticBezierTo(
         size.width / 2, size.height, size.width, size.height - 60);
 
+    path.lineTo(size.width, 0);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) => false;
+}
+
+class ClipInfoClass extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0, size.height);
+    path.lineTo(size.width - 100, size.height);
+    path.lineTo(size.width, size.height - 100);
     path.lineTo(size.width, 0);
     path.close();
 
