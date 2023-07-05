6cc38610702f063c95821b84332888903e98ebb0
regi <regi>
2023-07-05 23:34:08 +0700

    fix navigasi

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -46,140 +46,259 @@ class HomeView extends GetView<HomeController> {
               margin: const EdgeInsets.only(top: 20),
               child: Column(
                 children: [
-                  Container(
-                    height: Get.height * 0.42,
-                    child: LayoutBuilder(
-                        builder: (context, constraints) => Column(
+                  Column(
+                    children: [
+                      ClipPath(
+                          clipper: ClipInfoClass(),
+                          child: Container(
+                            padding: const EdgeInsets.all(15),
+                            margin: const EdgeInsets.symmetric(horizontal: 25),
+                            // height: 230,
+                            decoration: const BoxDecoration(
+                                gradient: LinearGradient(colors: [
+                              Color(0xFFE52D27),
+                              Color(0xFFB31217),
+                            ])),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                ClipPath(
-                                    clipper: ClipInfoClass(),
-                                    child: Container(
-                                      padding: const EdgeInsets.all(15),
-                                      margin: const EdgeInsets.symmetric(
-                                          horizontal: 25),
-                                      // height: 230,
-                                      decoration: const BoxDecoration(
-                                          gradient: LinearGradient(colors: [
-                                        Color(0xFFE52D27),
-                                        Color(0xFFB31217),
-                                      ])),
-                                      child: Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          Row(children: [
-                                            const Text(
-                                              "0121829821",
-                                              style: TextStyle(
-                                                  color: Colors.white,
-                                                  fontSize: 20,
-                                                  fontWeight: FontWeight.bold),
-                                            ),
-                                            Image.asset(
-                                                "assets/icons/SimPATI_Logo.png")
-                                          ]),
-                                          const SizedBox(height: 10),
-                                          const Text(
-                                            "Sisa Pulsa Anda",
-                                            style: TextStyle(
-                                                color: Colors.white,
-                                                fontSize: 15,
-                                                fontWeight: FontWeight.bold),
-                                          ),
-                                          const SizedBox(height: 10),
-                                          Row(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.spaceBetween,
-                                            children: [
-                                              const Text(
-                                                "Rp 34.000",
-                                                style: TextStyle(
-                                                    color: Colors.white,
-                                                    fontSize: 25,
-                                                    fontWeight:
-                                                        FontWeight.bold),
-                                              ),
-                                              ElevatedButton(
-                                                onPressed: () {},
-                                                style: ElevatedButton.styleFrom(
-                                                    backgroundColor:
-                                                        Colors.amber),
-                                                child: const Text(
-                                                  "Isi Pulsa",
-                                                  style: TextStyle(
-                                                      color: Colors.black,
-                                                      fontSize: 20,
-                                                      fontWeight:
-                                                          FontWeight.bold),
-                                                ),
-                                              )
-                                            ],
-                                          ),
-                                          const SizedBox(height: 10),
-                                          RichText(
-                                              text: const TextSpan(
-                                            text: "Berlaku seumur ",
-                                            style: TextStyle(
-                                                color: Colors.black,
-                                                fontSize: 16),
-                                            children: [
-                                              TextSpan(
-                                                  text: "hidup",
-                                                  style: TextStyle(
-                                                      color: Colors.white,
-                                                      fontSize: 16))
-                                            ],
-                                          )),
-                                          const SizedBox(height: 10),
-                                          Row(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.start,
-                                            children: [
-                                              const Text(
-                                                "Telkomsel Point",
-                                                style: TextStyle(
-                                                    color: Colors.white,
-                                                    fontSize: 16),
-                                              ),
-                                              SizedBox(width: 10),
-                                              Container(
-                                                padding:
-                                                    const EdgeInsets.symmetric(
-                                                        horizontal: 10,
-                                                        vertical: 5),
-                                                decoration: BoxDecoration(
-                                                  color: Colors.amber,
-                                                  borderRadius:
-                                                      BorderRadius.circular(10),
-                                                ),
-                                                child: const Text(
-                                                  "172",
-                                                  style: TextStyle(
-                                                      fontSize: 16,
-                                                      color: Colors.black,
-                                                      fontWeight:
-                                                          FontWeight.bold),
-                                                ),
-                                              )
-                                            ],
-                                          ),
-                                          const SizedBox(height: 10),
-                                        ],
+                                Row(children: [
+                                  const Text(
+                                    "0121829821",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Image.asset("assets/icons/SimPATI_Logo.png")
+                                ]),
+                                const SizedBox(height: 10),
+                                const Text(
+                                  "Sisa Pulsa Anda",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                const SizedBox(height: 10),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    const Text(
+                                      "Rp 34.000",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 25,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    ElevatedButton(
+                                      onPressed: () {},
+                                      style: ElevatedButton.styleFrom(
+                                          backgroundColor: Colors.amber),
+                                      child: const Text(
+                                        "Isi Pulsa",
+                                        style: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.bold),
                                       ),
-                                    )),
+                                    )
+                                  ],
+                                ),
+                                const SizedBox(height: 10),
+                                RichText(
+                                    text: const TextSpan(
+                                  text: "Berlaku seumur ",
+                                  style: TextStyle(
+                                      color: Colors.black, fontSize: 16),
+                                  children: [
+                                    TextSpan(
+                                        text: "hidup",
+                                        style: TextStyle(
+                                            color: Colors.white, fontSize: 16))
+                                  ],
+                                )),
+                                const SizedBox(height: 10),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    const Text(
+                                      "Telkomsel Point",
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 16),
+                                    ),
+                                    const SizedBox(width: 10),
+                                    Container(
+                                      padding: const EdgeInsets.symmetric(
+                                          horizontal: 10, vertical: 5),
+                                      decoration: BoxDecoration(
+                                        color: Colors.amber,
+                                        borderRadius: BorderRadius.circular(10),
+                                      ),
+                                      child: const Text(
+                                        "172",
+                                        style: TextStyle(
+                                            fontSize: 16,
+                                            color: Colors.black,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                const SizedBox(height: 10),
                               ],
-                            )),
+                            ),
+                          )),
+                      const SizedBox(height: 10),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 25),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            StatusCard(
+                              title: "Internet",
+                              data: "19.98",
+                              satuan: " GB",
+                            ),
+                            StatusCard(
+                              title: "Telepon",
+                              data: "19",
+                              satuan: " Menit",
+                            ),
+                            StatusCard(
+                              title: "SMS",
+                              data: "19",
+                              satuan: " SMS",
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      )
+                    ],
                   ),
                   Container(
-                    height: 10,
-                    color: Colors.purple,
+                    height: 7,
+                    color: Colors.grey[300],
                   ),
-                  Expanded(child: Container(color: Colors.blue))
+                  Expanded(
+                    child: Container(
+                      color: Colors.blue,
+                      child: Column(
+                        children: [
+                          //body
+                          Expanded(
+                            child: Container(),
+                          ),
+                          Container(
+                            height: 100,
+                            color: Colors.amber,
+                            child: const Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                ItemNav(
+                                  status: true,
+                                  title: "beranda",
+                                  icon: "heart",
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  )
                 ],
               ),
             )
           ],
         ));
+  }
+}
+
+class ItemNav extends StatelessWidget {
+  const ItemNav({
+    super.key,
+    required this.status,
+    required this.title,
+    required this.icon,
+  });
+
+  final bool status;
+  final String title;
+  final String icon;
+
+  final Map<String, IconData> iconDataMap = const {
+    "house": FontAwesomeIcons.house,
+    "heart": FontAwesomeIcons.heart,
+    "star": FontAwesomeIcons.star,
+    // Add more icon mappings as needed
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          height: 30,
+          child: FaIcon(
+            iconDataMap[icon],
+            color: (status == true) ? Colors.red : Colors.white,
+          ),
+        ),
+        const SizedBox(height: 5),
+        Text(
+          "Beranda",
+          style: TextStyle(color: (status == true) ? Colors.red : Colors.white),
+        ),
+      ],
+    );
+  }
+}
+
+class StatusCard extends StatelessWidget {
+  const StatusCard({
+    super.key,
+    required this.title,
+    required this.data,
+    required this.satuan,
+  });
+
+  final String title;
+  final String data;
+  final String satuan;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Container(
+        padding: const EdgeInsets.all(10),
+        width: Get.width * 0.27,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            RichText(
+              text: TextSpan(
+                text: data,
+                style: const TextStyle(fontSize: 20, color: Colors.black),
+                children: [
+                  TextSpan(
+                      text: satuan,
+                      style: const TextStyle(fontSize: 16, color: Colors.grey)),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
 
